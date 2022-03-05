9c58b71822137329a9a650430d0a5d2f381b2e37
NeoThokala <97616080+NeoThokala@users.noreply.github.com>
2022-03-04 23:09:53 -0500

    Update q2.py

---
+++ hw3/q2.py
@@ -0,0 +1,17 @@
+import csv
+with open('testfile.csv', 'r') as read_obj:
+csv_reader = csv.reader(read_obj)
+list_of_csv = list(csv_reader)
+print(list_of_csv)
+parts = list(csv.reader ([line]))[0]
+
+from otestfile.csv import Quantity
+from testfile.csv import UnitPrice
+
+class Country(MRJob):
+    def mapper(self, Country, line):
+        for Country in line:
+           yield (Country)
+    def reducer(self, key, amount):
+        amount = (Quantity, Price)
+        yield (Country, sum(amount))
